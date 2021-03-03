87920dde2c26c1fbdebbf7b51b4113c065173e7f
tamamo <tamamo@tamamono.moe>
2021-03-03 10:30:19 +0800

    Fix checking method.

--- launch.php
+++ launch.php
@@ -2,6 +2,7 @@
 chdir(__DIR__);
 print("root\n");
 
+function main() {
 //Check new redive (JP) version
 if (!file_exists('./redive/last_version')) {
   $last_version = array('TruthVersion'=>0,'hash'=>'');
@@ -59,7 +60,7 @@ curl_setopt_array($curl, array(
 $TruthVersion = $last_version['TruthVersion'];
 $current_ver = $TruthVersion|0;
 print("current_ver=".$current_ver."\n");
-for ($i=1; $i<=20; $i++) {
+for ($i=1; $i<=500; $i++) {
   $guess = str_pad($current_ver + $i * 1,8,'0',STR_PAD_LEFT);
   print("guess=".$guess."\n");
   curl_setopt($curl, CURLOPT_URL, 'https://img-pc.so-net.tw/dl/Resources/'.$guess.'/Jpn/AssetBundles/iOS/manifest/manifest_assetmanifest');
@@ -74,7 +75,7 @@ for ($i=1; $i<=20; $i++) {
 if ($TruthVersion == $last_version['TruthVersion']) {
   for ($i=0; $i<=20; $i++) {
     $guess = str_pad(round($current_ver,3,PHP_ROUND_HALF_DOWN) + 1000 + $i * 1,8,'0',STR_PAD_LEFT);
-    print("guess=".$guess."\n");
+    //print("guess=".$guess."\n");
     curl_setopt($curl, CURLOPT_URL, 'https://img-pc.so-net.tw/dl/Resources/'.$guess.'/Jpn/AssetBundles/iOS/manifest/manifest_assetmanifest');
     curl_exec($curl);
     $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
@@ -103,3 +104,10 @@ else{
   exec('php main.php 2>&1 | tee shell-logs/$(date +"%FT-%H-%M-%S").txt');
   chdir(__DIR__);
 }
+//unlink('.lock');
+}
+main();
+/*
+if (!file_exists('.lock')) {
+  main();
+}*/
